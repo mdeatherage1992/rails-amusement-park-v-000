2dcaf56413cad614f680372eee9b08d92673194f
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-08-20 14:12:46 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root "static_pages#home"
   get "/signin", to: "sessions#new"
   post "/sessions/create", to: "sessions#create"
-  delete "/signout", to: "sessions#destroy"
+  delete "/logout", to: "sessions#destroy"
   post "/rides/new", to: "rides#new"
   resources :attractions
   resources :users
